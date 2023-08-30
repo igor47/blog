c28b5280edb1996fb5d2a42ee67663043b4756bf
Igor Serebryany <igor47@moomers.org>
2023-08-30 13:25:11 -0700

    fix: custom element for bootstrapize
    defines at least the fields we want to modify

--- src/lib/bootstrap.ts
+++ src/lib/bootstrap.ts
@@ -4,9 +4,17 @@ import type { Plugin } from 'unified';
 import type { Node } from 'unist';
 import type { Element } from 'hast';
 
+interface HTMLElement extends Element {
+  data: {
+    hProperties: {
+      class: string[];
+    }
+  };
+}
+
 const bootstrapize: Plugin = () => {
   return (tree: Node) => {
-    visit(tree, 'blockquote', (node: Element) => {
+    visit(tree, 'blockquote', (node: HTMLElement) => {
       node.data = node.data || {}
       node.data.hProperties = node.data.hProperties || {}
       node.data.hProperties!.class = node.data.hProperties.class || []
